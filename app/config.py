a0c20a64adc15f992b7ed94ff460b8b4ef9bcd6b
fazalfarhan01 <fazal.farhan@gmail.com>
2021-11-13 23:05:38 +0530

    retaining old variable declaration

--- app/config.py
+++ app/config.py
@@ -2,3 +2,8 @@ import os
 AUTH = os.environ.get("AUTH")
 DELAY = int(os.environ.get("DELAY"))
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
+
+
+# AUTH = '' # Google Auth Cookie
+# DELAY = 120 # Time to wait after server update (60-120)
+# WEBHOOK_URL = '' # Discord Webhook URL
