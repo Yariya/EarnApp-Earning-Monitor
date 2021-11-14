075ce91b42dbd719031f0c6167da070719515c3f
Yariya <65712074+Yariya@users.noreply.github.com>
2021-11-14 13:13:23 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,10 +4,8 @@ auth = '' # Google Auth Cookie
 delay = 120 # Time to wait after server update (60-120)
 webhook_url = '' # Discord Webhook URL
 
-
+# Docker
 
 AUTH = (auth if os.environ.get("AUTH") is None else os.environ.get("AUTH"))
 DELAY = (delay if os.environ.get("DELAY") is None else int(os.environ.get("DELAY")))
 WEBHOOK_URL = (webhook_url if os.environ.get("WEBHOOK_URL") is None else os.environ.get("WEBHOOK_URL"))
-
-
