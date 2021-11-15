fb98c6d5e1e36f5af5f0dc34dfba2b1b3e4fc6e3
Yariya <65712074+Yariya@users.noreply.github.com>
2021-11-15 18:09:34 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-# Dev: Yariya
-
 import json
 import sys
 import time
@@ -9,6 +7,7 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 import requests
 from dp.baljson import *
 from datetime import datetime
+import colorama
 
 
 class Headers:
@@ -28,11 +27,21 @@ def main():
                         headers={'cookie': f'auth=1; auth-method=google; oauth-refresh-token={AUTH}'},
                         params=Headers.params)
 
-    # Lazy... Will update next update :)
-    x = str(devs.text).split(",")
-    devlen = int(round(len(x) / 8.9, 0))
-    devgnu = str(devs.text).count("sdk-node") / 2
-    devwin = str(devs.text).count("sdk-win") / 2
+    devlen = 0
+    devgnu = 0
+    devwin = 0
+    totalbw = 0
+
+    x = json.loads(devs.text)
+
+    for y in x:
+        devlen += 1
+        if str(y["uuid"]).startswith("sdk-win"):
+            devwin += 1
+        else:
+            devgnu += 1
+        totalbw += int(y["total_bw"])
+
     try:
         infos = requests.get("https://earnapp.com/dashboard/api/user_data",
                              headers={'cookie': f'auth=1; auth-method=google; oauth-refresh-token={AUTH}'},
@@ -49,22 +58,25 @@ def main():
                             headers={'cookie': f'auth=1; auth-method=google; oauth-refresh-token={AUTH}'},
                             params=Headers.params)
 
+    devices = requests.get('https://earnapp.com/dashboard/api/devices',
+                           headers={'cookie': f'auth=1; auth-method=google; oauth-refresh-token={AUTH}'},
+                           params=Headers.params)
+
     # Short fix of payment needed problem
     xbackup = []
     try:
         xbackup = welcome7_from_dict(json.loads(response.text))
     except:
-        print("No Payment details.")
         xbackup = "nil"
     x = json.loads(response.text)
     history = x["balance"]
 
-    print(f"{colors.GREEN}[+] Successfully loaded profile!{colors.RESET}")
-    print(f"    {colors.RED}Username: {y['name']}")
+    print(f"{colorama.Fore.GREEN}[+] Successfully loaded profile!{colorama.Fore.RESET}")
+    print(f"    {colorama.Fore.RED}Username: {y['name']}")
     print(f"    Multiplier: {x['multiplier']}x")
     print(f"    Current Balance: {x['balance']}$")
     print(f"    Lifetime Balance: {x['earnings_total']}$")
-    print(f"    Devices: {devlen} (Windows: {int(devwin)} Linux: {int(devgnu)})\n{colors.RESET}")
+    print(f"    Devices: {devlen} (Windows: {int(devwin)} Linux: {int(devgnu)})\n{colorama.Fore.RESET}")
 
     while 1:
         n = datetime.now().strftime("%H")
@@ -77,13 +89,26 @@ def main():
             response = requests.get('https://earnapp.com/dashboard/api/money',
                                     headers={'cookie': f'auth=1; auth-method=google; oauth-refresh-token={AUTH}'},
                                     params=Headers.params)
+
+            devs = requests.get('https://earnapp.com/dashboard/api/devices',
+                                headers={'cookie': f'auth=1; auth-method=google; oauth-refresh-token={AUTH}'},
+                                params=Headers.params)
+
             x = json.loads(response.text)
+            y = json.loads(devs.text)
+            totalbw2 = 0
+            devlen = 0
+            for n in y:
+                totalbw2 += int(n["total_bw"])
+                devlen += 1
             if x['balance'] > history:
+
                 webhook = DiscordWebhook(url=WEBHOOK_URL, rate_limit_retry=True)
                 embed = DiscordEmbed(title="Balance Updated!", description="Your EarnApp Balance has been updated!",
                                      color="03b2f8")
                 embed.set_thumbnail(url="https://www.androidfreeware.net/img2/com-earnapp.jpg")
                 embed.add_embed_field(name="Earned", value=f"+{round((x['balance'] - history), 2)}$")
+                embed.add_embed_field(name="Traffic", value=f"+{round(((totalbw2-totalbw)/pow(10,6)), 2)} mb")
                 embed.add_embed_field(name="Balance", value=f"{x['balance']}$")
                 embed.add_embed_field(name="Multiplier", value=f"{x['multiplier']}")
                 embed.add_embed_field(name="Total Earnings", value=f"{x['earnings_total']}$")
@@ -92,10 +117,11 @@ def main():
                 webhook.add_embed(embed)
                 webhook.execute()
 
-                print(f"{colors.GREEN}[+] Balance Updated!")
-                print(f"    +{round((x['balance'] - history), 2)}${colors.RESET}\n")
+                print(f"{colorama.Fore.GREEN}[+] Balance Updated!")
+                print(f"    +{round((x['balance'] - history), 2)}${colorama.Fore.RESET}\n")
 
                 history = float(x['balance'])
+                totalbw = totalbw2
 
 
 
@@ -110,9 +136,9 @@ def main():
                                  icon_url="https://img.icons8.com/color/64/000000/paypal.png")
                 webhook.add_embed(embed)
                 webhook.execute()
-                print(f"{colors.MAGENTA}[+] New payout detected!")
+                print(f"{colorama.Fore.MAGENTA}[+] New payout detected!")
                 print(f"    Payout: {round((history - x['balance']), 2)}")
-                print(f"    Payment method: {xbackup.redeem_details.payment_method}{colors.RESET}\n")
+                print(f"    Payment method: {xbackup.redeem_details.payment_method}{colorama.Fore.RESET}\n")
                 history = float(x['balance'])
 
             else:
@@ -122,13 +148,13 @@ def main():
                 embed.set_thumbnail(url="https://www.androidfreeware.net/img2/com-earnapp.jpg")
                 embed.add_embed_field(name="Earned", value=f"+{round((x['balance'] - history), 2)}$")
                 embed.add_embed_field(name="Multiplier", value=f"{x['multiplier']}")
-                embed.add_embed_field(name="Total Earnings", value=f"{x['earnings_total']}$")
+                embed.add_embed_field(name="Total Earnings", value=f"{x['total_earnings']}$")
                 embed.set_footer(text=f"You are earning with {devlen} Devices",
                                  icon_url="https://img.icons8.com/nolan/64/paypal.png")
                 webhook.add_embed(embed)
                 webhook.execute()
-                print(f"{colors.YELLOW}[~] Balance did not Change!")
-                print(f"    +{round((x['balance'] - history), 2)}${colors.RESET}\n")
+                print(f"{colorama.Fore.YELLOW}[~] Balance did not Change!")
+                print(f"    +{round((x['balance'] - history), 2)}${colorama.Fore.RESET}\n")
                 history = float(x['balance'])
 
 
